19a7816d2dfe6cf8718473810b6d27e6ebecd2a3
Jahtofunmi Osho <oshotofunmi@gmail.com>
2024-11-10 15:57:12 +0100

    font setup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [loaded, error] = useFonts({
+    HelveticaNeue: require('./assets/fonts/HelveticaNeueRoman.otf'),
+    'HelveticaNeue-Bold': require('./assets/fonts/HelveticaNeueBold.otf'),
+    'HelveticaNeue-Light': require('./assets/fonts/HelveticaNeueLight.otf'),
+    'HelveticaNeue-Medium': require('./assets/fonts/HelveticaNeueMedium.otf'),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
